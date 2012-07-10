a6516888e846029574f1e0bfe17fd20f1a01ddd1
hoang188 <hoang188@gmail.com>
2012-07-10 23:12:12 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapi::Application.config.secret_token = 'bb5dca69e949a21c93420cd772697951fc57916439dfcdeec8dfcc3042899a1c21d390aa9070825f1d206528991463dc125d99d96a2c178829c7bdfa84178198'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Demoapi::Application.config.session_store :cookie_store, key: '_demoapi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Demoapi::Application.config.session_store :active_record_store
